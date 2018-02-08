73ef7f58d1b0e8ded99b6ce99432e32fb3c27e7b
Wesley Penn <wesley.m.penn@gmail.com>
2018-02-08 12:37:35 -0500

    FirstCommit – UI integrated; HeartData Integrated; Awaiting Jessica's Piece

---
+++ Cardiapp/tagListViewController.swift
@@ -0,0 +1,70 @@
+//
+//  Created by Sam Lack on 1/14/18.
+//  Copyright © 2018 Riverdale Country School. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class tagListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableViewTagList: UITableView!
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var tags: [PersonalTag] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tags.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let tag = tags[indexPath.row]
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        let startDateAppear = dateFormatter.string(from: tag.startDate!)
+        let endDateAppear = dateFormatter.string(from: tag.endDate!)
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = "ACTIVITY: \((tag.activity)!)\nSTART TIME: \(startDateAppear)\nEND TIME: \(endDateAppear)\nSTARRED: \(tag.star)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let tag = tags[indexPath.row]
+            context.delete(tag)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do {
+                tags = try context.fetch(PersonalTag.fetchRequest())
+            } catch {
+                print("Fetching Failed")
+            }
+        }
+        tableViewTagList.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableViewTagList.delegate = self
+        tableViewTagList.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getData()
+        tableViewTagList.reloadData()
+    }
+    
+    func getData() {
+        do {
+            tags = try context.fetch(PersonalTag.fetchRequest())
+        } catch {
+            print("Fetching Failed")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
