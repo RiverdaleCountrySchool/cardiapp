8c7786e13cc72b13f76835adfa6490d74c44d6f0
Sam Lack <samlack@sams-macbook-pro-2.local>
2018-04-25 10:53:33 -0400

    custom cell added

---
+++ Cardiapp/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  Cardiapp
+//
+//  Created by Sam Lack on 3/8/18.
+//  Copyright © 2018 Riverdale Country School. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+}
