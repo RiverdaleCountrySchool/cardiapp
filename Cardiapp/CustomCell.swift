eb08cfeacf2353d21cb274859e4b30561c4178e6
Sam Lack <samlack@sams-macbook-pro-2.local>
2018-04-25 11:17:37 -0400

    merge handling --> pull (4/25/18)

--- Cardiapp/CustomCell.swift
+++ Cardiapp/CustomCell.swift
@@ -2,19 +2,9 @@
 //  CustomCell.swift
 //  Cardiapp
 //
-<<<<<<< HEAD
-//  Created by Sam Lack on 3/8/18.
-//  Copyright © 2018 Riverdale Country School. All rights reserved.
-//
 
 import UIKit
-=======
-//  Created by Wesley Penn on 4/8/18.
-//  Copyright © 2018 Riverdale Country School. All rights reserved.
-//
-
 import Foundation
->>>>>>> 0cf588f2f51679d924eb992b9d090bb7056e9646
 import JTAppleCalendar
 
 class CustomCell: JTAppleCell {
