84f938a25b96280e07896ecb5cd23200e922cd77
Wesley Penn <wesley.m.penn@gmail.com>
2018-05-18 00:12:48 -0400

    - fixed calendar view - added Heart Healthy Advice Viewcontroller

---
+++ Cardiapp/heartHealthyAdviceController.swift
@@ -0,0 +1,46 @@
+//
+//  heartHealthyAdviceController.swift
+//  Cardiapp
+//
+//  Created by Wesley Penn on 5/17/18.
+//  Copyright © 2018 Riverdale Country School. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class heartHealthyAdviceController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        var coreDataTags = [PersonalTag]
+//        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//        
+//        let fetchRequest: NSFetchRequest<PersonalTag> = PersonalTag.fetchRequest()
+//        
+//        do {
+//            let coreDataTags = try context.fetch(fetchRequest)
+//            return coreDataTags
+//        } catch {
+//            print("Fetching Failed")
+//            return nil
+//        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
