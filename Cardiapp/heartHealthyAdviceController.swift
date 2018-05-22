61b87a480d37b333d3fd2d311bb91874530908a9
Jessica Frank <jessicafrank@jessicas-macbook-air.local>
2018-05-22 10:34:16 -0400

    Creating the personalization of the heart healthy advice - Grouping all of the tags into categories and finding out which category has the most tags

--- Cardiapp/heartHealthyAdviceController.swift
+++ Cardiapp/heartHealthyAdviceController.swift
@@ -27,6 +27,8 @@ class heartHealthyAdviceController: UIViewController {
         for val in coreDataTags{
             parsedCoreData.append((val.activity, val.startDate, val.endDate, val.star))
         }
+        
+        cartegorizeTags(tags: parsedCoreData)
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,7 +37,55 @@ class heartHealthyAdviceController: UIViewController {
     }
     
     
+    func cartegorizeTags(tags:[(String?, Date?, Date?, Bool)] ){
+        
+        let sepTags = tags[0].0!.components(separatedBy: " ")
+        var activityText = sepTags[0]
+        
+        let tagList = tags[0].0!
+        
+        
     
+        for i in tagList{
+            
+            let j = String(i)
+            
+            //different types of activity categories
+            var active = 0
+            var sedentary = 0
+            var music = 0
+            var vices = 0
+            var eating = 0
+            
+            if (j == "Soccer ⚽️") || (j == "Running 🏃") || (j=="Basketball 🏀") || (j=="Football 🏈") || (j=="Baseball ⚾️") || (j=="Walking 🚶") || (j=="Lifting Weights 🏋️‍♀️") || (j=="Dancing 💃") || (j=="Tennis 🎾") || (j=="Volleyball 🏐") || (j=="Ping Pong 🏓") || (j=="Ice Hockey 🏒") || (j=="Field Hockey 🏑") || (j=="Archery 🏹") || (j=="Fishing 🎣") || (j=="Boxing 🥊") || (j=="Martial Arts 🥋") || (j=="Skiing ⛷") || (j=="Snowboarding 🏂") || (j=="Ice Skating ⛸") || (j=="Wrestling 🤼‍♀️") || (j=="Gymnastics 🤸‍♀️") || (j=="Golf 🏌️") || (j=="Surfing 🏄") || (j=="Water Polo 🤽‍♀️") || (j=="Swimming 🏊‍♀️") || (j=="Rowing 🚣‍♀️") || (j=="Horseback Riding 🏇") || (j=="Biking 🚴") || (j=="Mountain Biking 🚵‍♀️") || (j=="Juggling 🤹‍♂️") || (j=="Rugby 🏉") || (j=="Pool 🎱") || (j=="Badminton 🏸") || (j=="Cricket 🏏") || (j=="Bowling 🎳") || (j=="Darts 🎯") || (j=="Fencing 🤺") || (j=="Dodgeball 🤾‍♂️") {
+                active = active + 1
+            }
+            else if (j == "Eating 🍔"){
+                eating = eating + 1
+            }
+            else if (j == "Sleeping 💤") || (j=="Watching TV 📺") || (j=="Video Games 🎮"){
+                sedentary = sedentary + 1
+            }
+            else if (j=="Drinking 🍸") || (j=="Smoking 🚬"){
+                vices = vices + 1
+            }
+            else if (j=="Trumpet 🎺") || (j=="Piano 🎹") || (j=="Drums 🥁") || (j=="Saxophone 🎷") || (j=="Guitar 🎸") || (j=="Violin 🎻") || (j=="Singing 🎤"){
+                music = music + 1
+            }
+            
+            var categories = [active, sedentary, music, vices, eating]
+            categories.sorted()
+            
+        
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
 
     /*
     // MARK: - Navigation
